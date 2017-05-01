5cfe6fe1502fb857ed1221cdd83faf27b0f220ca
Sven Wille <wille.sven.public@gmail.com>
2017-05-01 23:29:23 +0200

    initial commit

---
+++ src/propLogic/Ex01.v
@@ -0,0 +1,17 @@
+
+(*Proof: A -> A*)
+
+
+Lemma Ex01_1 : forall A : Prop , A -> A .
+Proof.
+intros.
+exact H.
+Qed.
+
+(*using automation*)
+
+Lemma Ex01_2 :  forall A : Prop , A -> A .
+Proof.
+auto.
+Qed.
+
---
+++ src/propLogic/Ex02.v
@@ -0,0 +1,16 @@
+
+(*Proof: A -> B -> A*)
+
+Lemma Ex02_1 : forall (A B : Prop), A -> B -> A.
+Proof. 
+intros.
+exact H.
+Qed.
+
+
+(*using automation*)
+
+Lemma Ex02_2 : forall (A B : Prop), A -> B -> A.
+Proof.
+firstorder.  (*auto would be enough*)
+Qed.
